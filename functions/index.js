3d0c935bf772371d34458ed9d9ff5c63e5a06238
souvik <souvik.banerjee@innoraft.com>
2025-12-05 11:16:18 +0530

    ENV file added for security purposes.

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,75 @@ if (!admin.apps.length) {
 const firestore = admin.firestore();
 const messaging = admin.messaging();
 
+function buildClientFirebaseConfig() {
+  const runtimeConfig = functions.config() || {};
+  const clientConfig = runtimeConfig.client || runtimeConfig.firebase || {};
+  const config = {
+    apiKey:
+      process.env.FIREBASE_API_KEY ||
+      clientConfig.apiKey ||
+      clientConfig.api_key,
+    authDomain:
+      process.env.FIREBASE_AUTH_DOMAIN ||
+      clientConfig.authDomain ||
+      clientConfig.auth_domain,
+    projectId:
+      process.env.FIREBASE_PROJECT_ID ||
+      clientConfig.projectId ||
+      clientConfig.project_id,
+    storageBucket:
+      process.env.FIREBASE_STORAGE_BUCKET ||
+      clientConfig.storageBucket ||
+      clientConfig.storage_bucket,
+    messagingSenderId:
+      process.env.FIREBASE_MESSAGING_SENDER_ID ||
+      clientConfig.messagingSenderId ||
+      clientConfig.messaging_sender_id,
+    appId:
+      process.env.FIREBASE_APP_ID ||
+      clientConfig.appId ||
+      clientConfig.app_id,
+    measurementId:
+      process.env.FIREBASE_MEASUREMENT_ID ||
+      clientConfig.measurementId ||
+      clientConfig.measurement_id,
+  };
+
+  const required = [
+    "apiKey",
+    "authDomain",
+    "projectId",
+    "storageBucket",
+    "messagingSenderId",
+    "appId",
+  ];
+  const missing = required.filter((key) => !config[key]);
+  if (missing.length) {
+    throw new Error(
+      `Missing Firebase client config env vars: ${missing.join(", ")}`
+    );
+  }
+  return config;
+}
+
+exports.getFirebaseClientConfig = functions.https.onRequest((req, res) => {
+  res.set("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Vary", "Origin");
+  if (req.method === "OPTIONS") {
+    res.status(204).send("");
+    return;
+  }
+  try {
+    const config = buildClientFirebaseConfig();
+    res.status(200).json(config);
+  } catch (error) {
+    functions.logger.error("getFirebaseClientConfig failure", error);
+    res.status(500).json({ error: "Firebase config is not available." });
+  }
+});
+
 function safeStringify(value, options = {}) {
   const { maxStringLength = 500, pretty = false } = options;
   try {
